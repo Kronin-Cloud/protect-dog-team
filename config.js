f9e49480f74823e0f7302e8d95612ecdd6897592
Krisztian Banhidy <s4mur4i@users.noreply.github.com>
2018-10-08 13:52:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  title: 'Protect Dog Team', // Required
-  author: 'Protect Dog Team', // Required
+  title: 'Protection Dogs Team', // Required
+  author: 'Protection Dogs Team', // Required
   tagline: 'Cox Freya Molly Vodka',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://protect-dog.team',
-    instagram: 'https://twitter.com/protect-dog-team',
-    facebook: 'https://ca.linkedin.com/in/protect-dog-team'
+    website: 'https://protection-dogs.team',
+    instagram: 'https://twitter.com/protection-dogs-team',
+    facebook: 'https://ca.linkedin.com/in/protection-dogs-team'
   }
 };
